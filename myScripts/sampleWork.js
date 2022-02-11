e23a22429a5422d7b08c8541b4cee8983abf0ecd
dileepyadavgolla <dileepyadav3021@gmail.com>
2022-02-11 19:07:00 +0530

    first message

---
+++ myScripts/sampleWork.js
@@ -0,0 +1,24 @@
+let guessNode=document.getElementById("guessNum")
+let num=Math.floor(Math.random()*6);
+let count=0
+let resultNode=document.getElementById("result")
+let btnGuessNode=document.getElementById("btnGuess")
+let div3Node=document.getElementById("div3")
+let summaryNode=document.getElementById("summary")
+
+btnGuessNode.onclick=function(){
+    console.log(num)
+    console.log(guessNode.value)
+    if(guessNode.value==num){
+        resultNode.textContent="you are right go on!"
+        guessNode.value=""
+        count=count+1
+    }
+    else{
+        summaryNode.textContent="Game Completed"
+        resultNode.textContent="your score is "+count
+        guessNode.value=""
+        num=Math.floor(Math.random()*11);
+
+    }
+}
